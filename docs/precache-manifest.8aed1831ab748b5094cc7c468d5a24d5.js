39f1cedb8a57c260cdf292b0aca00cc86809894f
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2020-07-31 01:53:58 -0500

    docs agregado

---
+++ docs/precache-manifest.8aed1831ab748b5094cc7c468d5a24d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8626241ae5531f57782206682a326806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27d182ff68579c61bd5",
+    "url": "/static/css/main.57fc3999.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27d182ff68579c61bd5",
+    "url": "/static/js/main.13ab7884.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
